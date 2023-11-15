9632eb85b85ab773407b88dc815288fcebb5f3b5
Eric <jhonjhonjumaday@gmail.com>
2023-11-15 16:57:57 +0800

    start on basic parser, and initial imports

--- bits33asm/src/main.rs
+++ bits33asm/src/main.rs
@@ -1,3 +1,12 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+
+#[derive(Parser)]
+#[grammar = "bits33asm.pest"]
+struct BareParser;
+
 fn main() {
     println!("Hello, world!");
 }
